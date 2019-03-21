4bdd54006bf6dcae0c5288cecc46398af3b081a9
valentine-m <valentine-m@live.fr>
2019-03-21 01:42:17 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,20 @@
+def greeting
+  puts "Hello World"
+end
+greeting
+greeting
+greeting
+greeting
+greeting
+
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+stylish_chef
+
+!!"hi"
